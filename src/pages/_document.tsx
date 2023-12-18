743aef68178eb29b011b70c0477f653143cb4fcc
marcin <marcinpultorak@gmail.com>
2023-12-18 16:50:49 +0100

    favicon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,6 +5,24 @@ export default function Document() {
     <Html lang="en" className="scroll-smooth">
       <Head>
         {/* <meta name="color-scheme" content="only light" /> */}
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/images/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/images/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/images/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/images/site.webmanifest" />
         <title>
           Serwis Samochodowy Mierzyn | FHUPLUS - Mechanik Samochodowy
         </title>
